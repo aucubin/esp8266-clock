58e60ab0598fee95894bb8bc2f89f87c8be3640e
aucubin <aucubin@aucubin.de>
2021-12-22 12:07:54 +0100

    rewrite functionality with support for timezones and winter time. Use Platform.io instead of Arduino Sketches

--- clock/src/main.cpp
+++ clock/src/main.cpp
@@ -1,9 +1,79 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <TZ.h>
+#include <PolledTimeout.h>
+#include <TM1637Display.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const char *ssid = "ssid";
+const char *password = "password";
+
+const int clk_pin = D6;
+const int dio_pin = D5;
+const uint8_t dot_bitmask = 0b01000000;
+
+static esp8266::polledTimeout::periodicMs setTimeNow(1000);
+static esp8266::polledTimeout::periodicMs checkWifiNow(30000);
+static tm *local_now;
+
+TM1637Display display(clk_pin, dio_pin);
+
+void connectToWifi()
+{
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void calcTime()
+{
+  time_t now = time(nullptr);
+  local_now = localtime(&now);
+}
+
+void setTime()
+{
+  int hour = local_now->tm_hour;
+  int min = local_now->tm_min;
+
+  int time_num = (hour*100) + min;
+
+  display.showNumberDecEx(time_num, dot_bitmask, true);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  connectToWifi();
+
+  configTime(TZ_Europe_Berlin, "pool.ntp.org");
+
+  uint8_t data[] = {0xff, 0xff, 0xff, 0xff};
+  display.setBrightness(0);
+  display.setSegments(data);
+}
+
+void loop()
+{
+  if (setTimeNow)
+  {
+    calcTime();
+    setTime();
+  }
+
+  if(checkWifiNow){
+    if(WiFi.status() != WL_CONNECTED){
+      connectToWifi();
+    }
+  }
 }
